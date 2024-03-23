1a4e06d647df7c5a1f33fde05bda4e5d86ef6d03
Saffan Seyidoğlu <saffanduygun@gmail.com>
2024-03-23 22:58:02 +0300

    Update send_message.py

--- SendWpMessage/send_message.py
+++ SendWpMessage/send_message.py
@@ -1,11 +1,11 @@
 #bu bir whatsapp mesaj botudur
-#oncelikle terminale "pip install pyautogui" komutunu yazıp paketi yukluyoruz
+#oncelikle terminale "pip install pyautogui" komutunu yazip paketi yukluyoruz
 #kodu calistirdiktan sonra hemen whatsapp webe gelip yollayacagimiz kisi ile olan sohbet bolumunu acin
 
 import pyautogui
 import time
 
-time.sleep(3)#whatsapp web acmamız icin gereken sure
+time.sleep(3)#whatsapp web acmamiz icin gereken sure
 
 for i in range(100):#mesaji kac kere yollayacagiz
     mesaj = f"{i} yollanacak mesaj"#yollanacak mesaj
